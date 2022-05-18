dd1d37bb3869a403ee3f51710894f5a9008a2d7b
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-18 20:43:25 -0300

    :sparkles: feat: Implementa método create de task na camada services

---
+++ back-end/src/services/TaskService.ts
@@ -0,0 +1,19 @@
+import { Task, TaskSchema } from '../interfaces/TaskInterface';
+import Service, { ServiceError } from '.';
+import TaskModel from '../models/TaskModel';
+
+class TaskService extends Service<Task> {
+  constructor(model = new TaskModel()) {
+    super(model);
+  }
+
+  create = async (obj: Task): Promise<Task | ServiceError | null> => {
+    const valid = TaskSchema.safeParse(obj);
+    if (!valid.success) {
+      return { error: valid.error };
+    }
+    return this.model.create(obj);
+  };
+}
+
+export default TaskService;
