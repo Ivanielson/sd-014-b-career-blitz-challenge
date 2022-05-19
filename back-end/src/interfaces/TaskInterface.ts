e76d9ea8c4fcc269bbafb8f06f9bfc03edabf558
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-19 09:17:48 -0300

    :recycle: refactor: Adiciona o campo createdAt a interface de task

--- back-end/src/interfaces/TaskInterface.ts
+++ back-end/src/interfaces/TaskInterface.ts
@@ -2,7 +2,8 @@ import { z } from 'zod';
 
 export const TaskSchema = z.object({
   task: z.string().min(12),
-  status: z.string().optional().default('Pendente'),
+  status: z.string().optional(),
+  createdAt: z.date().optional(),
 });
 
 export type Task = z.infer<typeof TaskSchema>;
