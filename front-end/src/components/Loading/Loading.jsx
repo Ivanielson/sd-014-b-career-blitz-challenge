237de4cacaae0adfbef5f738f1ffac69c7bd4953
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-19 17:35:18 -0300

    :sparkles: feat: Implementa o componente Loading

---
+++ front-end/src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoadingRotate from './styledLoading';
+
+function Loading() {
+  return (
+    <LoadingRotate>
+      📝
+    </LoadingRotate>
+  );
+};
+
+export default Loading;
