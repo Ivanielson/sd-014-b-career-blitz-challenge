5f828ac23b0d048ea0ca7f0f10ddd7b1e473f2ae
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-19 08:27:30 -0300

    :sparkles: feat: Implementa classe CustomRouter com a rota '/GET /tasks'

---
+++ back-end/src/routes/Router.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import Controller from '../controllers';
+
+class CustomRouter<T> {
+  router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  addRoute(controller: Controller<T>, route: string = controller.route) {
+    this.router.get(route, controller.getAll);
+  }
+}
+
+export default CustomRouter;
