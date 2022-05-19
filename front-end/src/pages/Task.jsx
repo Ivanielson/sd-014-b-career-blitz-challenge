af4ce48271b5b5faab02d5ecda45175f364740cc
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-19 16:37:50 -0300

    :sparkles: feat: Adiciona formulário na página Task.jsx

--- front-end/src/pages/Task.jsx
+++ front-end/src/pages/Task.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TaskForm from '../components/Form/TaskForm';
 
 function Task() {
   return (
     <section>
       <h2>Lista de Tarefas</h2>
+      <TaskForm />
     </section>
   );
 }
