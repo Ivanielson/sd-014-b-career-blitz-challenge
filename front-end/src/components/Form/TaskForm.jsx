4d7688052f337735e1e38afae7ab50c53e42eef0
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-19 18:21:42 -0300

    refactor: método handleClick de TaskForm para controlar carregamento das tarefas com loading

--- front-end/src/components/Form/TaskForm.jsx
+++ front-end/src/components/Form/TaskForm.jsx
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { createTask } from '../../services/request';
+import TaskContext from '../../context/TaskContext';
 
 function TaskForm() {
   const [task, setTask] = useState('');
+  const { setLoading } = useContext(TaskContext);
   const valid = task.length < 12;
+  const MILLISECONDS = 1000;
 
   async function handleClick() {
     const body = { task };
     const newTask = await createTask('/tasks', body);
     if ('message' in newTask) {
+      setLoading(true);
       setTask('');
     }
+    setTimeout(() => {
+      setLoading(false);
+    }, MILLISECONDS);
   }
 
   return (
